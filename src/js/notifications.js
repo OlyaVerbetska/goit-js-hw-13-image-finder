12284c84b2ecccf532acc05c01639fc49f61e93b
Olga Verbetska <verbetska150@gmail.com>
2021-02-23 14:44:50 +0200

    pain with notification)

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -1,19 +1,45 @@
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
 
-import {info} from '@pnotify/core';
-
 import '@pnotify/bootstrap4/dist/PNotifyBootstrap4.css';
-import { defaultModules } from '@pnotify/core';
+import { info, Stack, defaultModules } from '@pnotify/core';
 import * as PNotifyBootstrap4 from '@pnotify/bootstrap4';
 defaultModules.set(PNotifyBootstrap4, {});
 
+import * as PNotifyFontAwesome5Fix from '@pnotify/font-awesome5-fix';
+import * as PNotifyFontAwesome5 from '@pnotify/font-awesome5';
+defaultModules.set(PNotifyFontAwesome5Fix, {});
+defaultModules.set(PNotifyFontAwesome5, {});
+// defaults.styling = 'bootstrap4';
+// defaults.icons = 'font-awesome';
+
+// function noImagesMessage() {
+//   info({
+//     title: 'Sorry',
+//     text: `We haven't images matching your request`,
+//     delay: 3000,
+//   });
+// }
+const myStack = new Stack({
+  dir1: 'down',
+  dir2: 'left',
+  firstpos1: 25,
+  firstpos2: 30,
+  context: document.body,
+});
+const noticeOptions = {
+  title: 'Serching for images',
+  text: 'Awaiting while downloading images from server',
+  stack: myStack,
+  delay: 2000,
+};
 
 function noImagesMessage() {
   info({
+    ...noticeOptions,
     title: 'Sorry',
     text: `We haven't images matching your request`,
-    delay: 1000,
+    delay: 2000,
   });
 }
 
